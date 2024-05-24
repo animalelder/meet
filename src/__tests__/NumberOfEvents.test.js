d8bf69b8abe79926835df70741acc7e35a551ab7
animalelder <wllearned@icloud.com>
2024-05-23 18:39:49 -0700

    Update NumberOfEvents.test.js
    This version of the test has ZERO eslint warnings or errors.

--- src/__tests__/NumberOfEvents.test.js
+++ src/__tests__/NumberOfEvents.test.js
@@ -1,25 +1,28 @@
-import { render } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import NumberOfEvents from '../components/NumberOfEvents';
 import userEvent from '@testing-library/user-event';
 
 describe('<NumberOfEvents /> component', () => {
   let NumberOfEventsComponent;
-  beforeEach(() => {
-    NumberOfEventsComponent = render(
-      <NumberOfEvents setCurrentNOE={() => {}} />
-    );
-  });
+  // beforeEach(() => {
+  //   NumberOfEventsComponent = render(
+  //     <NumberOfEvents setCurrentNOE={() => {}} />
+  //   );
+  // });
 
   test('has an element with "textbox" role', () => {
-    expect(NumberOfEventsComponent.queryByRole('textbox')).toBeInTheDocument();
+    render(<NumberOfEvents setCurrentNOE={() => {}} />);
+    expect(screen.getByRole('textbox')).toBeInTheDocument();
   });
 
   test('default value is 32', () => {
-    expect(NumberOfEventsComponent.queryByRole('textbox')).toHaveValue('32');
+    render(<NumberOfEvents setCurrentNOE={() => {}} />);
+    expect(screen.getByRole('textbox')).toHaveValue('32');
   });
 
   test('update numberOfEvents when user types', async () => {
-    const numberOfEvents = NumberOfEventsComponent.queryByRole('textbox');
+    render(<NumberOfEvents setCurrentNOE={() => {}} />);
+    const numberOfEvents = screen.getByRole('textbox');
     const user = userEvent.setup();
     await user.clear(numberOfEvents);
     await user.type(numberOfEvents, '10');
@@ -27,8 +30,8 @@ describe('<NumberOfEvents /> component', () => {
   });
 
   test('updates numberOfEvents only if input is a number', async () => {
-    const { queryByRole } = NumberOfEventsComponent;
-    const numberOfEventsInput = queryByRole('textbox');
+    render(<NumberOfEvents setCurrentNOE={() => {}} />);
+    const numberOfEventsInput = screen.getByRole('textbox');
     const user = userEvent.setup();
 
     // Attempt to enter a negative number
