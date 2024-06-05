56e2b29784318472dac2ec69b3ec0731b97edce9
animalelder <wllearned@icloud.com>
2024-06-04 19:04:46 -0700

    Added Atatus monitoring

--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ import './styles/output.css';
 import App from './App';
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import reportWebVitals from './reportWebVitals';
+import * as atatus from 'atatus-spa';
+atatus.config('06f41d836d374fa5be190d3793d64900').install();
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -21,3 +23,4 @@ serviceWorkerRegistration.unregister();
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+atatus.notify(new Error('Test Atatus Setup'));
